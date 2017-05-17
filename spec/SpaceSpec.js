88249b65f44a21b189672223c8d931fcffea68bf
Chris Mitchell <chrisjmit@gmail.com>
2017-05-17 17:12:21 +0100

    jasmine working

--- spec/SpaceSpec.js
+++ spec/SpaceSpec.js
@@ -24,3 +24,29 @@
       });
     });
   });
+
+  // 
+  // describe('Homepage Tests', function() {
+  //
+  //   var client = {};
+  //
+  //   before(function(done) {
+  //     client = webdriverio.remote({ desiredCapabilities: {browserName: 'chrome'}   });
+  //     client.init(done);
+  //   });
+  //
+  //   after(function(done) {
+  //     client.end(done);
+  //   });
+  //
+  //   it('Homepage Title Displays',function(done) {
+  //     client
+  //       .url('http://localhost:3000')
+  //       .getTitle(function(err, title) {
+  //         expect(err).to.not.be.true;
+  //         expect(title).to.eql('Example Title');
+  //       })
+  //       .call(done);
+  //   });
+  //
+  // });
