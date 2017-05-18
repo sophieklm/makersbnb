befe5d18d2aa90f1882200ee833a37ecae3116eb
Paula <pmuldoon@umich.edu>
2017-05-18 18:14:55 +0100

    refactored controller to include render

--- server/controllers/space.js
+++ server/controllers/space.js
@@ -3,26 +3,27 @@ const Space = require('../models').Space;
 module.exports = {
   create(req, res) {
     return Space
-      .create({
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-      })
-      .then(space => res.status(201).send(space))
-      .catch(error => res.status(400).send(error));
+    .create({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+    })
+    .then(space => res.status(201).send(space))
+    .catch(error => res.status(400).send(error));
   },
 
   retrieve(req, res) {
-  return Space
+    return Space
     .findAll()
-    .then(space => {
-      if (!space) {
+    .then(spaces => {
+      if (!spaces) {
         return res.status(404).send({
           message: 'Space Not Found',
         });
       }
-      return res.status(200).send(space);
+
+      return res.status(200).render('spaces/index.ejs', { spaces });
     })
     .catch(error => res.status(400).send(error));
-},
+  },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,22 +6,19 @@ module.exports = (app) => {
       res.render('index.ejs');
   });
 
-  app.post('/users/new', function(req,res){
-    userController.create(req,res);
-    res.redirect('/');
-  });
+  // app.post('/users/new', function  (req, res){
+  //   userController.create(req, res);
+  //   res.redirect('/');
+  // });
 
-  app.get('/spaces', function(req, res) {
-    var spaces = spaceController.retrieve(req,res);
-    res.render('spaces/index.ejs', {spaces});
-  });
+  app.get('/spaces', spaceController.retrieve);
 
-  app.get('/spaces/new', function(req, res) {
+  app.get('/spaces/new', function (req, res) {
     res.render('spaces/new.ejs');
   });
 
-  app.post('/spaces/new', function(req,res){
-    spaceController.create(req,res);
+  app.post('/spaces/new', function (req, res) {
+    spaceController.create(req, res);
     res.redirect('/spaces');
   });
 
