f238b7dfd784d183345cf958bb2913779016f6fb
Sam <sblausten@Sams-Mac.local>
2017-05-19 14:48:49 +0100

    Tests set up

--- spec/SpaceSpec.js
+++ spec/SpaceSpec.js
@@ -1,17 +1,17 @@
   // var request = require("request");
   // var base_url = "http://localhost:8000/";
   // var space;
-  //
+  
   // beforeEach(function(){
-  //
+  
   // });
-  //
+  
   // describe("Add a new space", function() {
   //   describe("GET /", function() {
-  //     it("returns status code 200", function(done) {
+  //     it("returns status code 200", function(next) {
   //       request.get(base_url, function(error, response, body) {
   //         expect(response.statusCode).toBe(200);
-  //         done();
+  //         next();
   //       });
   //     });
   //   });
