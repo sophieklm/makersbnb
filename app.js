dde50e5ac370a9ed6b00599441b1e9579a47e7d1
Sophie Mann <sklm@live.co.uk>
2017-05-19 11:42:32 +0100

    adds passport-local

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
 const app = express();
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: false }));
 
+// required for passport
+app.use(session({ secret: 'super_secret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 app.set('view engine', 'ejs');
 
-require('./server/routes')(app);
+require('./server/routes')(app, passport);
 app.get('*', (req, res) => res.status(200).send( {
   message: "Welcome to NodemonBnB!"
 }));
